a3bf46cffbb658a5d9851294d1704a3f17b0f144
Duran <k35400@eon.com>
2024-02-22 15:34:00 +0100

    refactored code and switched to huggingface model google/flan-t5-large. Not the best one, but it works

---
+++ utils/database_utils.py
@@ -0,0 +1,99 @@
+import os
+import pathlib
+import shutil
+
+from langchain.chains.query_constructor.schema import AttributeInfo
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.retrievers import SelfQueryRetriever, \
+    ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
+from langchain.vectorstores import Chroma
+
+
+class VectorDatabase:
+    def __init__(self,
+                 book_path: pathlib.Path,
+                 embeddings=HuggingFaceEmbeddings()):
+        """
+        Parameters
+        ----------
+        book_path:
+            path to the vector database, should be the same as the book path
+        """
+        self.vector_store_dir = str(book_path.joinpath('database'))
+        self.embeddings = embeddings
+
+        self.vector_db = None
+
+    def create_vector_store(self, splits, delete_existing_db=True):
+        if delete_existing_db and os.path.isdir(self.vector_store_dir):
+            shutil.rmtree(self.vector_store_dir)
+
+        self.vector_db = Chroma.from_documents(
+                documents=splits,
+                embedding=self.embeddings,
+                persist_directory=self.vector_store_dir
+        )
+        self.vector_db.persist()
+
+    def set_vector_store(self):
+        self.vector_db = Chroma(
+                persist_directory=self.vector_store_dir,
+                embedding_function=self.embeddings
+        )
+        # ToDo: add exception, if DB does not exist
+        return
+
+    def set_llm(self, llm):
+        self.llm = llm
+
+    def set_meta_data_fields(self):
+        query_metadata = [
+            AttributeInfo(
+                    name="source",
+                    description="The raw data, which contains the book.",
+                    type="string",
+            ),
+            AttributeInfo(
+                    name="chapter",
+                    description="The chapter from the book",
+                    type="integer",
+            ),
+            AttributeInfo(
+                    name="subchapter",
+                    description="The subchapter is part of the chapter and "
+                                "therefore hierarchically lower.",
+                    type="integer",
+            ),
+        ]
+        self.query_metadata = query_metadata
+
+
+class Retriever:
+    def __init__(self, llm, vector_db):
+        self.llm = llm
+        self.vector_db = vector_db
+        self.retriever = None
+
+    def set_retriever(self, retriever_type='self', query_metadata=None):
+        if retriever_type == 'self':
+            document_content_description = "Book"
+            retriever = SelfQueryRetriever.from_llm(
+                    self.llm,
+                    self.vector_db,
+                    document_content_description,
+                    query_metadata,
+                    verbose=True
+            )
+        elif retriever_type == 'compress':
+            compressor = LLMChainExtractor.from_llm(self.llm)
+            retriever = ContextualCompressionRetriever(
+                    base_compressor=compressor,
+                    base_retriever=self.vector_db.as_retriever()
+            )
+        elif retriever_type == 'vector_db':
+            retriever = self.vector_db.as_retriever(
+                    search_type='mmr',
+                    search_kwargs={"k": 6,
+                                   "fetch_k": 18})
+        self.retriever = retriever
--- watson.py
+++ watson.py
@@ -3,37 +3,96 @@ import pathlib
 
 import openai
 from dotenv import load_dotenv, find_dotenv
-from langchain.chat_models import ChatOpenAI
+from langchain.llms import HuggingFaceHub
+from langchain.prompts import PromptTemplate
 
 load_dotenv(find_dotenv())
 openai.api_key = os.environ['OPENAI_API_KEY']
 
 from utils import document_handler as dh
-from utils.document_handler import VectorDatabase
+from utils.database_utils import VectorDatabase, Retriever
+from utils import chat_utils
+from transformers import pipeline
 
-if __name__ == "__main__":
-    book_path = pathlib.Path('data/books/animal_farm')
+def get_all_documents_for_book(book_path):
+    """
+    gets all documents for book and returns the already splitted data
+    Parameters
+    ----------
+    book_path
+
+    Returns
+    -------
+
+    """
     loader_obj = dh.TextLoader(book_path)
-
     corpus = loader_obj.load_from_txt_file()
     loader_obj.set_splitter()
     splits = loader_obj.get_splits()
+    return splits
 
+
+if __name__ == "__main__":
+    # define which book you want to chat with
+    book_path = pathlib.Path('data/books/animal_farm')
+
+    # create vector_db object and determine if a vector db already exists or
+    # you want to create a new one
     vector_db_obj = VectorDatabase(book_path)
+    if os.path.exists(book_path.joinpath('database')):
+        vector_db_obj.set_vector_store()
+    else:
+        splits = get_all_documents_for_book(book_path)
+        vector_db_obj.create_vector_store(splits)
 
-    answer_obj = dh.AnswerMe(ChatOpenAI(model_name="gpt-3.5-turbo",
-                                        temperature=0.1,
-                                        max_tokens=1000),
-                             vector_db_obj.vector_db,
-                             vector_db_obj.retriever)
+    vector_db_obj.set_meta_data_fields()
+    # create llm
+    local_llm = HuggingFaceHub(
+            repo_id="google/flan-t5-large",
+            model_kwargs={
+                # "max_new_tokens": 200,
+                "temperature": 0.1
+            },
+    )
+
+    # model = AutoModelForSeq2SeqLM.from_pretrained(
+    #     "utils/model/fastchat-t5-3b-v1",
+    #     local_files_only=True,
+    #     # load_in_8bit=True,
+    #     device_map='auto',
+    #     low_cpu_mem_usage=True, offload_folder="utils/model/offload")
+    # pipe = pipeline("text2text-generation",
+    #                 model=model,
+    #                 tokenizer=tokenizer,
+    #                 max_length=512,
+    #                 temperature=0,
+    #                 top_p=0.95,
+    #                 repetition_penalty=1.15
+    #                 )
+    #
+    # local_llm = HuggingFacePipeline(pipeline=pipe)
+    #
+    # pipe = pipeline("text2text-generation",
+    #                 model=model,
+    #                 tokenizer=tokenizer)
+
+    # set retriever
+    retriever_obj = Retriever(llm=local_llm,
+                              vector_db=vector_db_obj.vector_db)
+    retriever_obj.set_retriever(retriever_type='vector_db',
+                                query_metadata=vector_db_obj.query_metadata)
+
+    answer_obj = chat_utils.AnswerMe(local_llm,
+                                     vector_db_obj.vector_db,
+                                     retriever_obj.retriever)
 
     # sample questions, that can be asked
-    question = "What animal is Napoleon?"
-    question = "What is the name of the farm, the animals live in? Only Use " \
-               "data from the first chapter"
+    # question = "What animal is Napoleon?"
+    # question = "What is the name of the farm, the animals live in? Only Use " \
+    #            "data from the first chapter"
     question = "What animal is Moses?"
-    question = "What does Napoleon represent in real life?"
-    question = "What does Boxer stand for in the book?"
+    # question = "What does Napoleon represent in real life?"
+    # question = "What does Boxer stand for in the book?"
 
     answer = answer_obj.answer(question)
 
