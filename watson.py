0278cd999024df4b2ac574dbc5f7da056ab67bb7
Duran <k35400@eon.com>
2024-03-21 10:15:38 +0100

    Added new book 1984 and modified the code a little bit

--- watson.py
+++ watson.py
@@ -11,7 +11,7 @@ from utils import chat_utils
 
 if __name__ == "__main__":
     # define which book you want to chat with
-    book_path = pathlib.Path('data/books/animal_farm')
+    book_path = pathlib.Path('data/books/1984')
 
     # create vector_db object and determine if a vector db already exists or
     # you want to create a new one
@@ -19,16 +19,16 @@ if __name__ == "__main__":
     if os.path.exists(book_path.joinpath('database')):
         vector_db_obj.set_vector_store()
     else:
-        splits = VectorDatabase.get_all_documents_for_book(book_path)
+        splits = vector_db_obj.get_all_documents_for_book()
         vector_db_obj.create_vector_store(splits)
 
     vector_db_obj.set_meta_data_fields()
     # create llm
     local_llm = HuggingFaceHub(
-            repo_id="google/flan-t5-large",
+            repo_id="google/flan-t5-xxl",
             model_kwargs={
                 # "max_new_tokens": 200,
-                "temperature": 0.1
+                "temperature": 0.01
             },
     )
 
@@ -41,12 +41,21 @@ if __name__ == "__main__":
                                      vector_db_obj.vector_db,
                                      retriever_obj.retriever)
 
-    # sample questions, that can be asked
+    # sample questions, that can be asked to animal farm
     question = "What animal is Moses?"
     # question = "What animal is Napoleon?"
     # question = "What is the name of the farm, the animals live in?"
     # question = "What was the name of the farm before that?"
     # question = "Why did Snowball leave the farm?"
+
+    question = "Who is the neighbour of Winston?"
+    question = "Who is Julia?"
+    question = "What is her relationship with Winston?"
+    question = "How did they get in touch?"
+    question = "What did the letter read?"
+
+    question = "Where does Winston work?"
+    question = "How many Ministeries do exist and what are their names?"
 
     answer = answer_obj.answer(question)
 
